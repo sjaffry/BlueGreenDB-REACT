cd7d3f0c901a51a9ba60926658684a78f965faae
Syed Jaffry <sajaffry@gmail.com>
2017-09-24 11:49:18 +1000

    Made dist work with npm start

--- src/dist/server.js
+++ src/dist/server.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var express = require('express');
 
 var app = express();
@@ -8,19 +6,21 @@ var port = process.env.PORT || 8000;
 
 var router = express.Router();
 
-router.route('/').get(function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+router.route('/')
+    .get(function(req,res){
+            res.sendFile(__dirname + '/index.html');
+    });    
 
 app.use(express.static(__dirname));
 app.use('/api', router);
-app.use(function (req, res, next) {
+app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
-app.listen(port, function () {
+
+app.listen(port, function(){
     console.log('Running on PORT ' + port);
 });
